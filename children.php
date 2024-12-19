ce93c27c609d4ca839c9fc7697fb6192eeff3bd8
ipb23.l.bluks <ipb23.l.bluks@pjvt.local>
2024-12-19 08:30:33 +0200

    kip pabeidzu

--- children.php
+++ children.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Children</title>
-    <link rel="stylesheet" href="styles_children.css">
+    <link rel="stylesheet" href="style_children.css">
 </head>
 <body> 
 <?php
